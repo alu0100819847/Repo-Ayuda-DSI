e3ee3112677efb91df6e138d194da8718b5c1f28
Carlos Castro <alu0100819847@ull.edu.es>
2017-02-25 13:31:36 +0000

    Despliegue

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,19 @@ var gulp = require('gulp')
 var shell = require('gulp-shell')
 var ghPages = require('gulp-gh-pages')
 
-gulp.task('b', shell.task([
-	'echo "hey"'
+gulp.task('build', shell.task([
+	'gitbook build txt html'
+	]));
+
+gulp.task('deploygh', shell.task([
+	'git add .',
+	'git commit -m "Despliegue"',
+	'git push origin master',
+	'git checkout gh-pages',
+	'git add .',
+	'git commit -m "Despliegue"',
+	'git push origin gh-pages',
+	'git checkout master'
 	]));
 
 gulp.task('deploy', function() {
